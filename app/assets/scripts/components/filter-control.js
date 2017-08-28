99041954c07c5ffdbda8f42371bf14bda68c3a2d
Nick Bumbarger <nbumbarger@gmail.com>
2017-08-27 20:27:28 -0400

    Use map-derived filters to populate checklist

--- app/assets/scripts/components/filter-control.js
+++ app/assets/scripts/components/filter-control.js
@@ -1,11 +1,49 @@
+// import React from 'react';
+// import _ from 'lodash';
+//
+// export const FilterControl = (props) => {
+//   return (
+//     <section className='filter-control'>
+//       <h2>Filters</h2>
+//       {_.map(props.filters, (filters, category) => {
+//         return (
+//           <div>
+//             <h3>{category}</h3>
+//             {filters.map((filter) => filter)}
+//           </div>
+//         )
+//       })}
+//     </section>
+//   );
+// };
+//
+// export default FilterControl;
+
 import React from 'react';
+import _ from 'lodash';
 
 export const FilterControl = (props) => {
-  console.log('control', props.filters)
   return (
     <section className='filter-control'>
       <h2>Filters</h2>
-      <p>{JSON.stringify(props.filters)}</p>
+      {_.map(props.filters, (filters, category) => {
+        return (
+          <div className='filters-inner'>
+            <h3>{category}</h3>
+            {filters.map((filter) => {
+              return (
+                <div className='filters__check-group' key={filter + '-check-group'}>
+                  <input
+                    type='checkbox'
+                    name={filter + '-check'}
+                    value={filter} />
+                  <label>{filter}</label>
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
     </section>
   );
 };
